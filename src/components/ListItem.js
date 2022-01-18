29d3223a2ccd7a3448b44d73a5c3ee5683523373
Fotios Tsakiris <footios76@gmail.com>
2022-01-18 20:21:15 +0200

    style movie details component

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import baseURL from "../constants/baseURL";
 import "../CSS/listItem.css";
+import MovieDetails from "./MovieDetails";
 
 const ListItem = ({ movie }) => {
-  const [url, setUrl] = useState();
+  const [url, setUrl] = useState("");
+  const [checkMovie, setCheckMovie] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -12,20 +14,27 @@ const ListItem = ({ movie }) => {
         // `${baseURL}configuration?api_key=<<api_key>>`
       );
       setUrl(response.url);
-      console.log(response.url);
     })();
   }, [url]);
 
-  return (
-    <li>
-      <img src={url} alt="movie image" />
-      <section className="movie-description">
-        <h4>{movie.title}</h4>
-        <h5>{movie.release_date}</h5>
-        <p>{movie.overview.slice(0, 88)}...</p>
-      </section>
-    </li>
-  );
+  const checkMovieHandler = () => {
+    console.log("checkMovieHandler", movie.id, url);
+    setCheckMovie(true);
+  };
+
+  if (checkMovie) {
+    return <MovieDetails movie={movie} />;
+  } else
+    return (
+      <li onClick={checkMovieHandler}>
+        <img src={url} alt="movie image" />
+        <section className="movie-description">
+          <h4>{movie.title}</h4>
+          <h5>{movie.release_date}</h5>
+          <p>{movie.overview.slice(0, 88)}...</p>
+        </section>
+      </li>
+    );
 };
 
 export default ListItem;
---
+++ src/components/MovieDetails.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import "../CSS/movieDetails.css";
+
+const MovieDetails = ({ movie }) => {
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        `http://image.tmdb.org/t/p/w185/${movie.poster_path}`
+        // `${baseURL}configuration?api_key=<<api_key>>`
+      );
+      setUrl(response.url);
+    })();
+  }, [url]);
+  console.log(movie.overview.length);
+
+  const getHeight = () => {
+    const len = movie.overview.length;
+    if (len < 100) return len + 740;
+    if (len > 100 && len < 200) return len + 710;
+    if (len > 200 && len < 200) return len + 700;
+    if (len > 200 && len < 300) return len + 650;
+    if (len > 300 && len < 400) return len + 600;
+    if (len > 400 && len < 500) return len + 520;
+    if (len > 600 && len < 700) return len + 520;
+    if (len > 700 && len < 800) return len + 350;
+    if (len > 800) return len + 350;
+  };
+  return (
+    <article className="container" style={{ height: `${getHeight()}px` }}>
+      <img src={url} alt="movie image" />
+      <section className="movie-description">
+        <h4>{movie.title}</h4>
+        <p>Release date: {movie.release_date}</p>
+        <p className="details">Populatity: {movie.popularity}</p>
+        <p className="details">Vote Average: {movie.vote_average}</p>
+        <p className="details">Vote count: {movie.vote_count}</p>
+        <p className="overview">{movie.overview}</p>
+      </section>
+    </article>
+  );
+};
+
+export default MovieDetails;
